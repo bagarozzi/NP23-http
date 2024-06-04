e09fe66ef1b4e957dfefbc5d66aa218d60dc2653
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-06-04 14:36:10 +0200

    Added comments

--- test.py
+++ test.py
@@ -3,14 +3,17 @@ import sys
 
 SERVER_DEFAULT_PORT = 8080
 
+# If a port is specified then use it, otherwise use the default port
 port = SERVER_DEFAULT_PORT
 if sys.argv[1:]:
     port = int(sys.argv[1:])
 
+# Build the address
 address = "http://localhost:" + str(port) + "/index.html"
 
 print("Testing POST and GET requests to: ", address)
 
+# Make requests
 try:
     get_request = requests.get(address)
     post_request = requests.post(address)
@@ -18,6 +21,7 @@ try:
 except requests.exceptions.Exception :
     print("Exception in making the test requests, make sure the server is running and has something to display")
 
+# Output the requests: 
 print("POST REQUEST: ",
     "\n Status code: ", post_request.status_code,
 )
